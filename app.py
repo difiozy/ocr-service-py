ed4d410e365ec54e519577c1a1fde5d86f661cfe
difiozy <shurow7@gmail.com>
2023-05-09 15:02:56 +0300

    remove useless html pages delete get route in recognize method

--- app.py
+++ app.py
@@ -21,12 +21,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/uploader', methods=['POST'])
 def upload_file():
     try:
         if request.method == 'POST':
